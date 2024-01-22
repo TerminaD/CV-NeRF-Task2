2845651b73bc77c5fe92a90cfb608e71c1cbdbb1
TerminaD <1499558179@qq.com>
2024-01-22 11:44:48 +0800

    Optimized project structure.

---
+++ utils/positional_encoding.py
@@ -0,0 +1,25 @@
+import torch
+
+class PositionalEncoding(torch.nn.Module):
+    def __init__(self, l):
+        """
+        Input:
+            l: number
+        """
+        super().__init__()
+        self.N_freqs = l
+        self.funcs = [torch.sin, torch.cos]
+        self.freq_bands = (2.0 ** torch.arange(l)) * torch.pi
+
+    def forward(self, x):
+        """
+        Input:
+            x: tensor
+        Output: tensor(x.size() * 2l, )
+        """
+        out = []
+        for freq in self.freq_bands:
+            for func in self.funcs:
+                out += [func(freq*x)]
+
+        return torch.cat(out, -1)
