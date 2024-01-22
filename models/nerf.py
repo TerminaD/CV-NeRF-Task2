7a0b0e662175d43529f2402ae4e5b01ef667256c
TerminaD <1499558179@qq.com>
2024-01-22 13:33:26 +0800

    Ported nerf-pl's NeRF class.

--- models/nerf.py
+++ models/nerf.py
@@ -1,2 +1,99 @@
-class NeRF:
-    pass
+import torch
+import torch.nn as nn
+
+
+class NeRF(nn.Module):
+    """
+    The neural network for NeRF.
+    
+    This class is ported from kwea123/nerf_pl 
+    (https://github.com/kwea123/nerf_pl/tree/master)
+    under the MIT license.
+    """
+    def __init__(self,
+                 D=8, 
+                 W=256,
+                 in_channels_xyz=60, 
+                 in_channels_dir=24, 
+                 skips=[4]):
+        """
+        D: number of layers for density (sigma) encoder
+        W: number of hidden units in each layer
+        in_channels_xyz: number of input channels for xyz (3*10*2=60 by default)
+        in_channels_dir: number of input channels for direction (3*4*2=24 by default)
+        skips: add skip connection in the Dth layer
+        """
+        super(NeRF, self).__init__()
+        self.D = D
+        self.W = W
+        self.in_channels_xyz = in_channels_xyz
+        self.in_channels_dir = in_channels_dir
+        self.skips = skips
+
+        # xyz encoding layers
+        for i in range(D):
+            if i == 0:
+                layer = nn.Linear(in_channels_xyz, W)
+            elif i in skips:
+                layer = nn.Linear(W+in_channels_xyz, W)
+            else:
+                layer = nn.Linear(W, W)
+            layer = nn.Sequential(layer, nn.ReLU(True))
+            setattr(self, f"xyz_encoding_{i+1}", layer)
+        self.xyz_encoding_final = nn.Linear(W, W)
+
+        # direction encoding layers
+        self.dir_encoding = nn.Sequential(
+                                nn.Linear(W+in_channels_dir, W//2),
+                                nn.ReLU(True))
+
+        # output layers
+        self.sigma = nn.Linear(W, 1)
+        self.rgb = nn.Sequential(
+                        nn.Linear(W//2, 3),
+                        nn.Sigmoid())
+        
+
+    def forward(self, x, sigma_only=False):
+        """
+        Encodes input (xyz+dir) to rgb+sigma (not ready to render yet).
+        For rendering this ray, please see rendering.py
+
+        Inputs:
+            x: (B, self.in_channels_xyz(+self.in_channels_dir))
+               the embedded vector of position and direction
+            sigma_only: whether to infer sigma only. If True,
+                        x is of shape (B, self.in_channels_xyz)
+
+        Outputs:
+            if sigma_ony:
+                sigma: (B, 1) sigma
+            else:
+                out: (B, 4), rgb and sigma
+        """
+        if not sigma_only:
+            input_xyz, input_dir = \
+                torch.split(x, [self.in_channels_xyz, self.in_channels_dir], dim=-1)
+        else:
+            input_xyz = x
+
+        xyz_ = input_xyz
+        for i in range(self.D):
+            if i in self.skips:
+                xyz_ = torch.cat([input_xyz, xyz_], -1)
+            xyz_ = getattr(self, f"xyz_encoding_{i+1}")(xyz_)
+
+        sigma = self.sigma(xyz_)
+        if sigma_only:
+            return sigma
+
+        xyz_encoding_final = self.xyz_encoding_final(xyz_)
+
+        dir_encoding_input = torch.cat([xyz_encoding_final, input_dir], -1)
+        dir_encoding = self.dir_encoding(dir_encoding_input)
+        rgb = self.rgb(dir_encoding)
+
+        out = torch.cat([rgb, sigma], -1)
+
+        return out
+    
