cb093ae4789c33dd62044492eb3c02938d793068
termina <1499558179@qq.com>
2024-01-25 08:54:37 +0800

    Debugging WIP

--- utils/psnr.py
+++ utils/psnr.py
@@ -1,11 +1,11 @@
-import numpy as np
+import torch
 
 def psnr_func(gt_img, pred_img):
-    mse = np.mean((gt_img - pred_img) ** 2) 
+    mse = torch.mean((gt_img - pred_img) ** 2) 
     
     # MSE is zero means no noise is present in the signal, therefore PSNR have no importance. 
     if(mse == 0):  
         return 100
     
-    psnr = 20 * np.log10(1/np.sqrt(mse)) 
+    psnr = 20 * torch.log10(1/torch.sqrt(mse)) 
     return psnr 
